52149ffb6eb1a964868bc86ef26b8b7c0e67a8b9
Andres Islas <hir.islas@outlook.com>
2020-10-19 18:53:27 -0500

    Se configura Routes en App y se migra a Home

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -3,10 +3,10 @@ import Test from '../components/Test';
 
 import '../assets/styles/App.scss';
 
-const App = () => (
+const Home = () => (
   <div className='app'>
     <Test />
   </div>
 );
 
-export default App;
+export default Home;
